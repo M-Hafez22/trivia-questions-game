f619741770d455e7ad8b3c39b91e85fc34f73a75
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-30 08:34:30 +0200

    Display Score

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ScoreContext } from "../contexts/scoreContext";
 
 function Score() {
+  const [{ score }, setScore] = useContext(ScoreContext);
+
   return (
-    <div>Score</div>
-  )
+    <div>
+      <h1>Score</h1>
+      <p>questions: {score.questions}</p>
+      <p>correct: {score.correct}</p>
+      <p>failed: {score.failed}</p>
+      <p>skiped: {score.skiped}</p>
+    </div>
+  );
 }
 
-export default Score
+export default Score;
