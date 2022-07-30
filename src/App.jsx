871c06f6d09cccb696a95f7afc7b7ba6847eb614
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-30 05:48:16 +0200

    ✨  Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Categroties from "./components/Categroties";
+import Question from "./components/Question";
+import Score from "./components/Score";
 
 function App() {
   return (
     <div className="App">
-      <h1>Trivia Questions game</h1>
+      <header className="App-header">Trivia Questions game</header>
+      <Router>
+        <div>
+          <Link to="/">Welcome</Link>
+          <Link to="/categroties">Categroties</Link>
+          <Link to="/question">question</Link>
+          <Link to="/score">score</Link>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/categroties" element={<Categroties />} />
+          <Route path="/question/:id" element={<Question />} />
+          <Route path="/score" element={<Score />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Categroties.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Categroties() {
+  return (
+    <div>
+      <h1>Categroties</h1>
+    </div>
+  );
+}
+
+export default Categroties;
