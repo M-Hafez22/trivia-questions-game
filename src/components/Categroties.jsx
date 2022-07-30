cd9f9701f3bd3b86489f37669cf60340913182c1
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-30 16:34:11 +0200

    📝 Document functions

--- src/components/Categroties.jsx
+++ src/components/Categroties.jsx
@@ -4,24 +4,31 @@ import { useQuery } from "@tanstack/react-query";
 import { SelectedCategoriesContext } from "../contexts/selectedCategories";
 
 function Categroties() {
+  // Chosen category name 
   const [value, setValue] = useState("");
+  // Chosen category ID
   const [id, setId] = useState(15);
 
   const [{ selectedCategories }, setSelectedCategories] = useContext(
     SelectedCategoriesContext
   );
 
-  // Display categorties
+  // Fetch categorties from API
   const { isLoading, error, data } = useQuery(["data"], () =>
     fetch("https://opentdb.com/api_category.php").then((res) => res.json())
   );
 
-  // choose random category ID
+  // choose random category (Name, ID)
   useEffect(() => {
-    const randomIndex = Math.floor(Math.random() * data?.trivia_categories?.length + 1);
-    setSelectedCategories([...selectedCategories, data?.trivia_categories?.[randomIndex]?.name])
-    setId(data?.trivia_categories?.[randomIndex]?.id)
-  },[data])
+    const randomIndex = Math.floor(
+      Math.random() * data?.trivia_categories?.length + 1
+    );
+    setSelectedCategories([
+      ...selectedCategories,
+      data?.trivia_categories?.[randomIndex]?.name,
+    ]);
+    setId(data?.trivia_categories?.[randomIndex]?.id);
+  }, [data]);
   // handle Loading
   if (isLoading) return "Loading...";
   // handle Error
@@ -29,30 +36,32 @@ function Categroties() {
   // Get data
   const categories = data?.trivia_categories;
   const categoriesNames = categories?.map((category) => category.name);
-  // const categoriesIds = categories.map((category) => category.id);
+  // Unchosen Categories
   const viewCategory = categoriesNames?.filter(
     (c) => !selectedCategories.includes(c)
   );
+  // Choose Category
   const handleClick = (e) => {
     setValue(e.target.innerText);
     setId(categories?.[categoriesNames?.indexOf(e.target.innerText)]?.id);
   };
+  // Add selected category to selected array
   const handleSubmit = (event) => {
     event.preventDefault();
-   setSelectedCategories([...selectedCategories, value]);
+    setSelectedCategories([...selectedCategories, value]);
   };
   return (
     <div>
       <h1>Categroties</h1>
-      <p>
-        Now Choose the Question Categroties:{value} - ID:{id}
-      </p>
+      <p>Now Choose the Question Categroties</p>
       <ul>
-        {viewCategory ?   viewCategory?.map((category, i) => (
-          <li key={i}>
-            <button onClick={handleClick}>{category}</button>
-          </li>
-        )) : "Loading"}
+        {viewCategory
+          ? viewCategory?.map((category, i) => (
+              <li key={i}>
+                <button onClick={handleClick}>{category}</button>
+              </li>
+            ))
+          : "Loading"}
       </ul>
       <button onClick={handleSubmit}>
         {viewCategory ? (
