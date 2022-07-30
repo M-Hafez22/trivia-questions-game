8b43b6b627e80e467984a7b0f57e3dce8cf9044e
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-30 06:13:20 +0200

    Fetch the Categories API

--- src/components/Categroties.jsx
+++ src/components/Categroties.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 function Categroties() {
+  // Display categorties
+  const { isLoading, error, data } = useQuery(["data"], () =>
+    fetch("https://opentdb.com/api_category.php").then((res) => res.json())
+  );
+  // handle Loading
+  if (isLoading) return "Loading...";
+  // handle Error
+  if (error) return "An error has occurred: " + error.message;
+  // Get data
+  const categories = data.trivia_categories;
+  console.dir(categories)
   return (
     <div>
       <h1>Categroties</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,19 @@ import App from "./App";
 // Contexts
 import { UserNameProvider } from "./contexts/userName";
 import { ChosenDifficultyProvider } from "./contexts/difficulty";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import './index.css'
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserNameProvider>
       <ChosenDifficultyProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
       </ChosenDifficultyProvider>
     </UserNameProvider>
   </React.StrictMode>
