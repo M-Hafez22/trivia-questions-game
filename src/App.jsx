d8904ddae067743dba5f483232f7b8cd2a8b8429
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-31 08:18:48 +0200

    💄 Highlight active link

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
 import Welcome from "./components/Welcome";
 import Categroties from "./components/Categroties";
 import Question from "./components/Question";
@@ -11,18 +17,33 @@ function App() {
       <header className="App-header">Trivia Questions game</header>
       <Router>
         <div className="links">
-          <Link to="/">Welcome</Link>
-          <Link to="/categroties">Categroties</Link>
-          <Link to="/score">score</Link>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "")}
+            to="/"
+          >
+            Welcome
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "")}
+            to="/categroties"
+          >
+            Categroties
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "")}
+            to="/score"
+          >
+            score
+          </NavLink>
         </div>
-      <main>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/categroties" element={<Categroties />} />
-          <Route path="/question/:category/:index" element={<Question />} />
-          <Route path="/score" element={<Score />} />
-        </Routes>
-      </main>
+        <main>
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/categroties" element={<Categroties />} />
+            <Route path="/question/:category/:index" element={<Question />} />
+            <Route path="/score" element={<Score />} />
+          </Routes>
+        </main>
       </Router>
     </div>
   );
