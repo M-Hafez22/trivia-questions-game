c2f752ff55d2b9c83ec1502347785ebad3b912ff
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-31 14:22:50 +0200

    :whale: Add Dockerfile

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/trivia-questions-game/',
+  server: {
+    port: 4000
+  },
   plugins: [react()]
 })
